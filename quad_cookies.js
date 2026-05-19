1a541de92faacb388a9982c1bdb4ab15eb2de328
hirata <jonathanhirata@gmail.com>
2026-05-19 15:06:16 -0700

    try sleep again

--- quad_cookies.js
+++ quad_cookies.js
@@ -1,11 +1,15 @@
 "use strict";
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 var M = Game.ObjectsById[7].minigame;
 var tower = Game.ObjectsById[7];
 var fthof = M.spellsById[1];
 
 M.castSpell(fthof);
 tower.sell(806);
-await new Promise(r => setTimeout(r, 1000));
+await sleep(1000);
 M.castSpell(fthof);
 tower.buy(410);
