2082320223faca6be8fcfa4822243dff276a2de3
hirata <jonathanhirata@gmail.com>
2026-05-19 15:43:00 -0700

    sleep

--- quad_cookies.js
+++ quad_cookies.js
@@ -15,12 +15,14 @@ async function gogogo() {
     M.castSpell(fthof);
     tower.buy(410);
 
+    await sleep(100);
     grimoireLumpRefill.click();
     promptOption0.click();
 
+    await sleep(100);
     M.castSpell(fthof);
     tower.sell(480);
-    await sleep(10000); // I need to click on the golden cookies on screen
+    await sleep(1000); // I need to click on the golden cookies on screen
     M.castSpell(fthof);
 }
 
