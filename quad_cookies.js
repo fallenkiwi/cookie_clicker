ec6f7521ce1720a9703ad8aee2ed017e2b9eeb77
hirata <jonathanhirata@gmail.com>
2026-05-19 15:15:18 -0700

    in a function

--- quad_cookies.js
+++ quad_cookies.js
@@ -4,16 +4,14 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function sleep1000() {
-    await sleep(1000);
-}
-
 var M = Game.ObjectsById[7].minigame;
 var tower = Game.ObjectsById[7];
 var fthof = M.spellsById[1];
 
-M.castSpell(fthof);
-tower.sell(806);
-sleep1000();
-M.castSpell(fthof);
-tower.buy(410);
+async function gogogo() {
+    M.castSpell(fthof);
+    tower.sell(806);
+    await sleep(1000);
+    M.castSpell(fthof);
+    tower.buy(410);
+}
