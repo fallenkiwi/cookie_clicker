46eaa87faf64c8ee69e194140dc62ca6b56157a4
hirata <jonathanhirata@gmail.com>
2026-05-19 14:58:33 -0700

    sell and buy

--- quad_cookies.js
+++ quad_cookies.js
@@ -1,7 +1,10 @@
 "use strict";
 
-var M=Game.ObjectsById[7].minigame;
+var M = Game.ObjectsById[7].minigame;
 var tower = Game.ObjectsById[7];
+var fthof = M.spellsById[1];
 
-M.castSpell(M.spellsById[1]);
+M.castSpell(fthof);
 tower.sell(806);
+M.castSpell(fthof);
+tower.buy(410);
