cfff5894c84e394aad6af2b38d6df95ec06e5ca1
hirata <jonathanhirata@gmail.com>
2026-05-20 22:21:18 -0700

    rm unused buildings

--- quad_cookies.js
+++ quad_cookies.js
@@ -7,22 +7,8 @@ function sleep(ms) {
 var M = Game.ObjectsById[7].minigame;
 var fthof = M.spellsById[1];
 
-var mine = Game.ObjectsById[3];
-var factory = Game.ObjectsById[4];
 var bank = Game.ObjectsById[5];
-var temple = Game.ObjectsById[6];
 var tower = Game.ObjectsById[7];
-var shipment = Game.ObjectsById[8];
-var alchemy = Game.ObjectsById[9];
-var portal = Game.ObjectsById[10];
-var time_machine = Game.ObjectsById[11];
-var antimatter = Game.ObjectsById[12];
-var prism = Game.ObjectsById[13];
-var chancemaker = Game.ObjectsById[14];
-var fractal = Game.ObjectsById[15];
-var javascript_console = Game.ObjectsById[16];
-var idleverse = Game.ObjectsById[17];
-var cortex = Game.ObjectsById[18];
 
 async function gogogo() {
     M.castSpell(fthof);
