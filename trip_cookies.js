fe99aff627265115d71bdc127c170c08432cbb18
hirata <jonathanhirata@gmail.com>
2026-06-01 13:33:35 -0700

    less sleep

--- trip_cookies.js
+++ trip_cookies.js
@@ -13,7 +13,7 @@ var tower = Game.ObjectsById[7];
 
 async function gogogo() {
     M.castSpell(gambler);
-    await sleep(2000);
+    await sleep(1000);
     tower.sell(296);
     await sleep(500);
     M.castSpell(fthof);
