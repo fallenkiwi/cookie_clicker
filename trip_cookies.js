7a036be8ec0d258a8982236d92c7ebb26ecdb838
hirata <jonathanhirata@gmail.com>
2026-06-01 13:17:37 -0700

    test

--- trip_cookies.js
+++ trip_cookies.js
@@ -14,18 +14,18 @@ var tower = Game.ObjectsById[7];
 async function gogogo() {
     M.castSpell(gambler);
     tower.sell(279);
-    await sleep(500);
-    M.castSpell(fthof);
-
-    await sleep(500);
-    grimoireLumpRefill.click();
-    promptOption0.click();
-
-    M.castSpell(haggler);
-    await sleep(500);
-    Game.season = 'easter';
-    await sleep(500);
-    M.castSpell(fthof);
+    // await sleep(500);
+    // M.castSpell(fthof);
+    //
+    // await sleep(500);
+    // grimoireLumpRefill.click();
+    // promptOption0.click();
+    //
+    // M.castSpell(haggler);
+    // await sleep(500);
+    // Game.season = 'easter';
+    // await sleep(500);
+    // M.castSpell(fthof);
 }
 
 gogogo();
